afa8cf0044766b3039f5e9bfacd9932af1b9a8ce
Roee Shapira <roy.shap1@gmail.com>
2019-12-29 00:52:37 +0200

    Added controller fns.

--- packages/back/server/src/sevices/sensors/sensors.service.ts
+++ packages/back/server/src/sevices/sensors/sensors.service.ts
@@ -83,4 +83,22 @@ export class SensorsService implements OnModuleInit {
 	): Promise<si.Systeminformation.ServicesData[]> {
 		return si.services(matcher);
 	}
+	public getDiskLayout(): Promise<si.Systeminformation.DiskLayoutData[]> {
+		return si.diskLayout();
+	}
+	public getBlockDevices(): Promise<si.Systeminformation.BlockDevicesData[]> {
+		return si.blockDevices();
+	}
+	public getDisksIO(): Promise<si.Systeminformation.DisksIoData> {
+		return si.disksIO();
+	}
+	public getFsSize(): Promise<si.Systeminformation.FsSizeData[]> {
+		return si.fsSize();
+	}
+	public getFsOpenFiles(): Promise<si.Systeminformation.FsOpenFilesData[]> {
+		return si.fsOpenFiles();
+	}
+	public getFsStats(): Promise<si.Systeminformation.FsStatsData> {
+		return si.fsStats();
+	}
 }
