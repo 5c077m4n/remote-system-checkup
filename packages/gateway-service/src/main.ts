6e4a1edd788a40283b4a1776b6328c2f33e5d0b4
Roee Shapira <roy.shap1@gmail.com>
2019-12-30 18:11:07 +0200

    Added rabbitmq connection.

--- packages/gateway-service/src/main.ts
+++ packages/gateway-service/src/main.ts
@@ -1,18 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import * as helmet from 'helmet';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+	FastifyAdapter,
+	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  app.use(helmet());
-  await app.listen(3000, '0.0.0.0');
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+	app.connectMicroservice({
+		transport: Transport.RMQ,
+		options: {
+			urls: [`amqp://localhost:5672`],
+			queue: 'gateway_queue',
+			queueOptions: { durable: false },
+		},
+	});
+	app.use(helmet());
+
+	await app.startAllMicroservicesAsync();
+	await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
