b98c5c9cedf788d5cfe301d7615286dcf660680f
Roee Shapira <roy.shap1@gmail.com>
2020-01-06 01:16:47 +0200

    Added missing dep.

--- packages/back/apps/auth-service/src/app.service.ts
+++ packages/back/apps/auth-service/src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+	@MessagePattern({ cmd: 'isAuthenticated' })
 	isUserAllowed({ username, password }): boolean {
 		return username && password;
 	}
