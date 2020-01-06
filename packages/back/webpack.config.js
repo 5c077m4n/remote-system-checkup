f7dcadc649d946684c238dec306fcb2b33462806
Roee Shapira <roy.shap1@gmail.com>
2020-01-06 14:29:02 +0200

    Improved webpack config.

--- packages/back/webpack.config.js
+++ packages/back/webpack.config.js
@@ -6,7 +6,7 @@ const nestOptions = require('./nest-cli.json');
 
 module.exports = function(options) {
 	const isProd = options.mode === 'production';
-	const entries =
+	const entry =
 		options.entry ||
 		Object.fromEntries(
 			Object.entries(nestOptions.projects).map(([key, val]) => [
@@ -15,23 +15,26 @@ module.exports = function(options) {
 			]),
 		);
 
-	return isProd
-		? {
-				...options,
-				entry: entries,
-				externals: [],
-				plugins: [...options.plugins],
-		  }
-		: {
-				...options,
-				entry: { ...entries, index: 'webpack/hot/poll?100' },
-				watch: true,
-				externals: [
-					nodeExternals({ whitelist: ['webpack/hot/poll?100'] }),
-				],
-				plugins: [
-					...options.plugins,
-					new webpack.HotModuleReplacementPlugin(),
-				],
-		  };
+	if (isProd) {
+		return {
+			...options,
+			entry,
+			externals: [],
+			plugins: [...options.plugins],
+		};
+	} else {
+		return {
+			...options,
+			entry:
+				typeof entry === 'string'
+					? { index: 'webpack/hot/poll?100', main: entry }
+					: { index: 'webpack/hot/poll?100', ...entry },
+			watch: true,
+			externals: [nodeExternals({ whitelist: ['webpack/hot/poll?100'] })],
+			plugins: [
+				...options.plugins,
+				new webpack.HotModuleReplacementPlugin(),
+			],
+		};
+	}
 };
