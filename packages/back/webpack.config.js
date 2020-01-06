432eb4a6f6561f96cac300fd8f2ffdb8a2fa1d28
Roee Shapira <roy.shap1@gmail.com>
2020-01-06 12:35:43 +0200

    Improved docker compose and webpack cofig.

--- packages/back/webpack.config.js
+++ packages/back/webpack.config.js
@@ -5,23 +5,33 @@ const nodeExternals = require('webpack-node-externals');
 const nestOptions = require('./nest-cli.json');
 
 module.exports = function(options) {
-	return {
-		...options,
-		entry: options.entry || {
-			index: 'webpack/hot/poll?100',
-			...Object.fromEntries(
-				Object.entries(nestOptions.projects).map(([key, val]) => [
-					key,
-					path.resolve(val.sourceRoot, val.entryFile) + '.ts',
-				]),
-			),
-		},
-		watch: true,
-		externals: [
-			nodeExternals({
-				whitelist: ['webpack/hot/poll?100'],
-			}),
-		],
-		plugins: [...options.plugins, new webpack.HotModuleReplacementPlugin()],
-	};
+	const isProd = options.mode === 'production';
+	const entries =
+		options.entry ||
+		Object.fromEntries(
+			Object.entries(nestOptions.projects).map(([key, val]) => [
+				key,
+				path.resolve(val.sourceRoot, val.entryFile) + '.ts',
+			]),
+		);
+
+	return isProd
+		? {
+				...options,
+				entry: entries,
+				externals: [],
+				plugins: [...options.plugins],
+		  }
+		: {
+				...options,
+				entry: { ...entries, index: 'webpack/hot/poll?100' },
+				watch: true,
+				externals: [
+					nodeExternals({ whitelist: ['webpack/hot/poll?100'] }),
+				],
+				plugins: [
+					...options.plugins,
+					new webpack.HotModuleReplacementPlugin(),
+				],
+		  };
 };
