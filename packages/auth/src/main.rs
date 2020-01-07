02a5d969f3d5e502ab8c542e33ab28d4ae0e5d0d
Roee Shapira <roy.shap1@gmail.com>
2020-01-08 00:11:52 +0200

    Added default logging.

--- packages/auth/src/main.rs
+++ packages/auth/src/main.rs
@@ -1,12 +1,21 @@
+#![allow(unused_variables)]
 #[macro_use]
 extern crate log;
 
 use amiquip::{
 	Connection, ConsumerMessage, ConsumerOptions, Exchange, QueueDeclareOptions, Result,
 };
+use env_logger::Env;
+
+fn init_logger() {
+	let env = Env::default()
+		.filter_or("MY_LOG_LEVEL", "info")
+		.write_style_or("MY_LOG_STYLE", "always");
+	env_logger::init_from_env(env);
+}
 
 fn main() -> Result<()> {
-	env_logger::init();
+	init_logger();
 	let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
 	let channel = connection.open_channel(None)?;
 	let exchange = Exchange::direct(&channel);
@@ -22,7 +31,7 @@ fn main() -> Result<()> {
 				consumer.ack(delivery)?;
 			}
 			other => {
-				println!("Consumer ended: {:?}", other);
+				info!("Consumer ended: {:?}", other);
 				break;
 			}
 		}
