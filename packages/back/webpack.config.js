b21ef8315b7e9dd393eb8426b2908e587c7efde6
Roee Shapira <roy.shap1@gmail.com>
2020-01-07 16:36:47 +0200

    Improved webpack config.

--- packages/back/webpack.config.js
+++ packages/back/webpack.config.js
@@ -4,44 +4,26 @@ const nodeExternals = require('webpack-node-externals');
 
 const nestOptions = require('./nest-cli.json');
 
-const WEBPACK_HOT_MODULE = 'webpack/hot/poll?100';
-
 module.exports = function(options) {
 	const isProd = options.mode === 'production';
-
-	let entry = options.entry;
-	if (!isProd) {
-		if (typeof entry === 'string') {
-			entry = [WEBPACK_HOT_MODULE, entry];
-		} else if (Array.isArray(entry) && entry[0] !== WEBPACK_HOT_MODULE) {
-			entry = [WEBPACK_HOT_MODULE].concat(entry);
-		}
-	}
-	if (!entry) {
-		entry = Object.fromEntries(
-			Object.entries(nestOptions.projects).map(([key, val]) => {
-				const projPath = path
-					.resolve(val.sourceRoot, val.entryFile)
-					.concat('.ts');
-
-				return [
-					key,
-					isProd ? projPath : [WEBPACK_HOT_MODULE].concat(projPath),
-				];
-			}),
+	const entry =
+		options.entry ||
+		Object.fromEntries(
+			Object.entries(nestOptions.projects).map(([key, val]) => [
+				key,
+				path.resolve(val.sourceRoot, val.entryFile).concat('.ts'),
+			]),
 		);
-	}
-
 	const plugins = isProd
 		? [...options.plugins]
 		: [...options.plugins, new webpack.HotModuleReplacementPlugin()];
-	const whitelist = isProd ? [] : [WEBPACK_HOT_MODULE];
 
 	return {
 		...options,
 		entry,
 		watch: !isProd,
-		externals: [nodeExternals({ whitelist })],
+		watchOptions: { aggregateTimeout: 300, poll: 500 },
+		externals: [nodeExternals({ whitelist: [] })],
 		plugins,
 	};
 };
