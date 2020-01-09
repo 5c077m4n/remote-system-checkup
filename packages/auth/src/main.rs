2ab5b25d87e4cf940ddc5d394b848c779c330652
Roee Shapira <roy.shap1@gmail.com>
2020-01-10 00:16:19 +0200

    Added rust code.

--- packages/auth/src/main.rs
+++ packages/auth/src/main.rs
@@ -1,41 +1,43 @@
-#![allow(unused_variables)]
 #[macro_use]
 extern crate log;
 
 use amiquip::{
-	Connection, ConsumerMessage, ConsumerOptions, Exchange, QueueDeclareOptions, Result,
+    Connection, ConsumerMessage, ConsumerOptions, Exchange, QueueDeclareOptions, Result,
 };
 use env_logger::Env;
 
 fn init_logger() {
-	let env = Env::default()
-		.filter_or("MY_LOG_LEVEL", "info")
-		.write_style_or("MY_LOG_STYLE", "always");
-	env_logger::init_from_env(env);
+    let env = Env::default()
+        .filter_or("MY_LOG_LEVEL", "info")
+        .write_style_or("MY_LOG_STYLE", "always");
+    env_logger::init_from_env(env);
 }
 
 fn main() -> Result<()> {
-	init_logger();
-	let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
-	let channel = connection.open_channel(None)?;
-	let exchange = Exchange::direct(&channel);
-	let queue = channel.queue_declare("AUTH_QUEUE", QueueDeclareOptions::default())?;
-	let consumer = queue.consume(ConsumerOptions::default())?;
-	info!("The auth service is now listening.");
+    init_logger();
+    let mut connection = Connection::insecure_open("amqp://guest:guest@localhost:5672")?;
+    let channel = connection.open_channel(None)?;
+    let _exchange = Exchange::direct(&channel);
+    let queue = channel.queue_declare("AUTH_QUEUE", QueueDeclareOptions::default())?;
+    let consumer = queue.consume(ConsumerOptions::default())?;
+    info!("The auth service is now listening.");
 
-	for (i, message) in consumer.receiver().iter().enumerate() {
-		match message {
-			ConsumerMessage::Delivery(delivery) => {
-				let body = String::from_utf8_lossy(&delivery.body);
-				info!("({:>3}) Received [{}]", i, body);
-				consumer.ack(delivery)?;
-			}
-			other => {
-				info!("Consumer ended: {:?}", other);
-				break;
-			}
-		}
-	}
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                if delivery.routing_key == "isAuth" {
+                    info!("isAuth");
+                }
+                let body = String::from_utf8_lossy(&delivery.body);
+                info!("({:>3}) Received [{}]", i, body);
+                consumer.ack(delivery)?;
+            }
+            other => {
+                info!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
 
-	connection.close()
+    connection.close()
 }
